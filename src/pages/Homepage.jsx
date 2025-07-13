b8822f7a1dafcc2ce6bd17c94956c7f7694f0a2f
Psalmstrings <psalmstrings@gmail.com>
2025-07-13 12:07:23 -0700

    fix and error

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Herobanner from '../component/herobanner'
+import Herobanner from '../component/Herobanner'
 import Navbar from '../component/Navbar'
 import Footer from '../component/Footer'
 import MovieCard from '../component/MovieCard'
